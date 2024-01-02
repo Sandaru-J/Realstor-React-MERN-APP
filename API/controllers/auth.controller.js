1db4c7cbb27a7229c074f7cf3131a7ff99b49260
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2024-01-02 13:56:27 +0530

    sign in fun

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -18,11 +18,13 @@ export const signup = async (req,res, next)=>{
 
 export const signin = async (req, res, next) =>{
     const {email, password} = req.body;
+    // console.log(email,password)
     try{
         const validUser = await User.findOne({email});
         if (!validUser)
             return next(errorHandler(404,'User not found!'));
-        const validPassword = (password,validUser.password)
+        // const validPassword = (password,validUser.password)
+        const validPassword = password === validUser.password
         if (!validPassword)
             return next(errorHandler(401, 'Wrong Credentials!'))
         const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET)
