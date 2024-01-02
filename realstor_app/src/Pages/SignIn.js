f419830a4040354bf4f1c7427b2bb7ed3aa007f6
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2024-01-02 19:29:08 +0530

    add redux tolkit/persist

--- realstor_app/src/Pages/SignIn.js
+++ realstor_app/src/Pages/SignIn.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const httpTarget = 'http://localhost:3000'
   const [formData,setFormData] = useState({})
-  const [error,setError] = useState(null);
-  const [loading, setLoading] = useState(false)
+  const { loading, error} = useSelector((state) => state.user);
+  //const [error,setError] = useState(null);
+  //const [loading, setLoading] = useState(false)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) =>{
     setFormData(
@@ -21,7 +25,7 @@ export default function SignIn() {
   const handleSubmit = async (e) =>{
     e.preventDefault();
     try {
-      setLoading(true)
+      dispatch(signInStart())
       const res = await fetch(`${httpTarget}/API/auth/sign-in`, 
       {
         method:'POST',
@@ -32,17 +36,14 @@ export default function SignIn() {
       })
       const data = await res.json();
       if(data.success === false){
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message))
         return;
       }
-        setLoading(false);
-        setError(null);
+        dispatch(signInSuccess(data))
         navigate('/')
     }catch(error)
     {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message))
     }
   };
   return (
